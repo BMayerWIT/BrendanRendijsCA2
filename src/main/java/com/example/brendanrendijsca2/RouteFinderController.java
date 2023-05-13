28d120e7fd8ee35bfe9088177879fec257d0e583
BrendanMayer <brendan.mayer1234@gmail.com>
2023-05-13 16:03:40 +0100

    destination and start station names fill comboboxes

--- src/main/java/com/example/brendanrendijsca2/RouteFinderController.java
+++ src/main/java/com/example/brendanrendijsca2/RouteFinderController.java
@@ -3,26 +3,22 @@ package com.example.brendanrendijsca2;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.image.*;
-import javafx.scene.paint.Color;
-import javafx.stage.FileChooser;
-import javafx.stage.Window;
+import javafx.scene.image.ImageView;
 
-
-public class RouteFinderController {
+    public class RouteFinderController {
 
         @FXML
-        private ComboBox<?> EndPoint;
+        private ComboBox<String> EndPoint;
 
         @FXML
         private ImageView ImageView;
@@ -31,7 +27,7 @@ public class RouteFinderController {
         private Label number;
 
         @FXML
-        private ComboBox<?> startPoint;
+        private ComboBox<String> startPoint;
 
         @FXML
         private Label welcomeText;
@@ -43,11 +39,6 @@ public class RouteFinderController {
 
         @FXML
         void SelectStartPoint(ActionEvent event) {
-
-        }
-
-        @FXML
-        void ShowFastestRoute(ActionEvent event) {
 
         }
 
@@ -65,17 +56,15 @@ public class RouteFinderController {
         void UploadImage(ActionEvent event) {
 
         }
-    Image image;
 
 
-    List<Station> stationList = new ArrayList<>();
 
-
-    public List<Station> fillListWithStations() {
+    List<GraphNodes<Station>> stationList = new ArrayList<>();
+    @FXML
+    public void ShowFastestRoute(ActionEvent event) throws Exception{ //fillListWithStations
         String filepath = "C:\\Users\\brend\\IdeaProjects\\BrendanRendijsCA2\\src\\main\\java\\com\\example\\brendanrendijsca2\\London.csv";
         String splitCSVBy = ",";
 
-        try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
             String line ="";
             line = bufferedReader.readLine();
@@ -83,22 +72,13 @@ public class RouteFinderController {
                 String [] splitArray = line.split(splitCSVBy);
 
                 Station station = new Station(Integer.parseInt(splitArray[0]), Float.parseFloat(splitArray[1]), Float.parseFloat(splitArray[2]), splitArray[3]);
-                stationList.add(station);
+                GraphNodes<Station> stationGN = new GraphNodes<>(station);
+                stationList.add(stationGN);
+                startPoint.getItems().add(splitArray[3]);
+                EndPoint.getItems().add(splitArray[3]);
             }
-            System.out.println(stationList);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("No File Found");
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            System.out.println("IOE EXCEPTION");
-            e.printStackTrace();
-        }
-        return stationList;
+            bufferedReader.close();
     }
-
-
 
 
 }
