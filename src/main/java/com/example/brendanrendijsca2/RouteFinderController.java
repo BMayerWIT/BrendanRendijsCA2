7480a875208b428b1da9ceca049f0d0ab75ca915
BrendanMayer <brendan.mayer1234@gmail.com>
2023-05-13 21:05:57 +0100

    destination and start station names fill comboboxes

--- src/main/java/com/example/brendanrendijsca2/RouteFinderController.java
+++ src/main/java/com/example/brendanrendijsca2/RouteFinderController.java
@@ -8,11 +8,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 
     public class RouteFinderController {
@@ -31,6 +34,8 @@ import javafx.scene.image.ImageView;
 
         @FXML
         private Label welcomeText;
+        @FXML
+        private ListView routeView;
 
         @FXML
         void SelectEndPoint(ActionEvent event) {
@@ -56,12 +61,16 @@ import javafx.scene.image.ImageView;
         void UploadImage(ActionEvent event) {
 
         }
+        public void ShowFastestRoute(ActionEvent event) {
+
+        }
 
 
 
     List<GraphNodes<Station>> stationList = new ArrayList<>();
+    Map<Integer, List<Station>> stationsById = new HashMap<>();
     @FXML
-    public void ShowFastestRoute(ActionEvent event) throws Exception{ //fillListWithStations
+    public void ReadCSVStationData(ActionEvent event) throws Exception{ //fillListWithStations
         String filepath = "C:\\Users\\brend\\IdeaProjects\\BrendanRendijsCA2\\src\\main\\java\\com\\example\\brendanrendijsca2\\London.csv";
         String splitCSVBy = ",";
 
@@ -74,10 +83,70 @@ import javafx.scene.image.ImageView;
                 Station station = new Station(Integer.parseInt(splitArray[0]), Float.parseFloat(splitArray[1]), Float.parseFloat(splitArray[2]), splitArray[3]);
                 GraphNodes<Station> stationGN = new GraphNodes<>(station);
                 stationList.add(stationGN);
+                createNodeEdges();
                 startPoint.getItems().add(splitArray[3]);
                 EndPoint.getItems().add(splitArray[3]);
             }
+            createNodeEdges();
             bufferedReader.close();
+    }
+
+    public void createNodeEdges() throws Exception {
+        String filepath = "C:\\Users\\brend\\IdeaProjects\\BrendanRendijsCA2\\src\\main\\java\\com\\example\\brendanrendijsca2\\Lines.csv";
+        String splitCSVBy = ",";
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+        String line = "";
+        line = bufferedReader.readLine();
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] values = line.split(splitCSVBy);
+            int startID = Integer.parseInt(values[0]);
+            int destinationID = Integer.parseInt(values[1]);
+            int lineID = Integer.parseInt(values[2]);
+
+
+            GraphNodes<Station> startNode = null;
+            GraphNodes<Station> destinationNode = null;
+            for (GraphNodes<Station> node : stationList) {
+
+                if (node.station.getID() == startID) {
+                    startNode = node;
+                }
+                if (node.station.getID() == destinationID) {
+                    destinationNode = node;
+                }
+            }
+
+            if (startNode != null && destinationNode != null) {
+                startNode.connectToNodeUndirected(destinationNode, lineID);
+            }
+
+            int lineNumber = -1;
+            for (GraphNodes<Station> node : stationList) {
+                if (node.station.getID() == startID) {
+                    lineNumber = lineID;
+                    break;
+                }
+            }
+
+            if (lineNumber != -1) {
+                List<Station> stationLineNumber = stationsById.get(lineNumber);
+                if (stationLineNumber == null) {
+                    stationLineNumber = new ArrayList<>();
+                    stationsById.put(lineNumber, stationLineNumber);
+                }
+                Station startStation = startNode.station;
+                Station endStation = destinationNode.station;
+
+                if (!stationLineNumber.contains(startStation)) {
+                    stationLineNumber.add(startStation);
+                }
+                if (endStation != null && !stationLineNumber.contains(endStation)) {
+                    stationLineNumber.add(endStation);
+                }
+            }
+
+        }
     }
 
 
